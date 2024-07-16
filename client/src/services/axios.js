b575e67054e59c6f424d1c3af678dab0254fb491
Anthony Gorski <anthony.developpeurweb@gmail.com>
2024-07-16 13:43:53 +0200

    🐛 (axios.js): fix base URL concatenation to include "/api" for consistency in API routes 📝 (axios.js): update API routes to remove "/api" prefix for better readability and maintainability ✨ (axios.js): add new functions to update, delete, like, dislike, and mark support as done in the support API routes

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -1,32 +1,31 @@
 import axios from "axios";
 
 const instance = axios.create({
-	baseURL: import.meta.env.VITE_API_URL || "http://localhost:5500",
+	baseURL:
+		`${import.meta.env.VITE_API_URL}/api` || "http://localhost:5500/api",
 	withCredentials: true,
 });
 
 /**
  * All routes related to auth
  */
-
 export const register = async (user) => {
-	return await instance.post("/api/register", user);
+	return await instance.post("/register", user);
 };
 
 export const login = async (user) => {
-	return await instance.post("/api/login", user);
+	return await instance.post("/login", user);
 };
 
 /**
  * All routes related to sessions
  */
 export const createSession = async (session) => {
-	return await instance.post("/api/sessions", session);
+	return await instance.post("/sessions", session);
 };
 
 export const getSessions = async () => {
-	// send cookie token to authorize the request
-	return await instance.get("/api/sessions");
+	return await instance.get("/sessions");
 };
 
 export const getSessionById = async (id) => {
@@ -48,15 +47,34 @@ export const joinSession = async (id) => {
 /**
  * All routes related to supports
  */
-
 export const createSupport = async (support) => {
-	return await instance.post("/api/supports", support);
+	return await instance.post("/supports", support);
 };
 
 export const getSupports = async () => {
-	return await instance.get("/api/supports");
+	return await instance.get("/supports");
 };
 
 export const getSupportById = async (id) => {
 	return await instance.get(`/supports/${id}`);
+};
+
+export const updateSupport = async (id, support) => {
+	return await instance.put(`/supports/${id}`, support);
+};
+
+export const deleteSupport = async (id) => {
+	return await instance.delete(`/supports/${id}`);
+};
+
+export const isLikeSupport = async (id) => {
+	return await instance.post(`/supports/${id}/like`);
+};
+
+export const isDislikeSupport = async (id) => {
+	return await instance.post(`/supports/${id}/dislike`);
+};
+
+export const isDoneSupport = async (id) => {
+	return await instance.post(`/supports/${id}/is_done`);
 };
