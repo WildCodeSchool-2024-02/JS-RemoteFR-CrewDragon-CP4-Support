e96ac379cfc896e212751d0702e0d33cf3d15264
Anthony Gorski <anthony.developpeurweb@gmail.com>
2024-09-18 19:57:02 +0200

    📝 (Register.jsx): Update input field placeholders for name, email, and password to provide examples and improve user experience.

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -45,13 +45,14 @@ function Register() {
 									htmlFor="email"
 									className="block text-sm font-medium leading-6 text-gray-900"
 								>
-									Adresse email
+									Votre nom
 								</label>
 								<div className="mt-2">
 									<input
 										id="name"
 										name="name"
 										type="text"
+										placeholder="John Doe"
 										required
 										className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
 										value={user.name}
@@ -64,13 +65,14 @@ function Register() {
 									htmlFor="email"
 									className="block text-sm font-medium leading-6 text-gray-900"
 								>
-									Votre nom
+									Votre email
 								</label>
 								<div className="mt-2">
 									<input
 										id="email"
 										name="email"
 										type="email"
+										placeholder="provider@xyz.com"
 										required
 										autoComplete="email"
 										className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -92,6 +94,7 @@ function Register() {
 										id="password"
 										name="password"
 										type="password"
+										placeholder="********"
 										required
 										autoComplete="current-password"
 										className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
